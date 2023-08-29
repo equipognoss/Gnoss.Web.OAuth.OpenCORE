45362fcb1bda98515bad0a301b35c35fe8025aa6
davidruiz-gnoss <davidruiz@gnoss.com>
2023-08-29 09:09:42 +0200

    Security Improvement

--- Gnoss.Web.OAuth/Program.cs
+++ Gnoss.Web.OAuth/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Gnoss.Web.OAuth
 {
@@ -15,6 +16,7 @@ namespace Gnoss.Web.OAuth
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
                 });
     }
 }
